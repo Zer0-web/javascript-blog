12e057cde3c939c1117bb0b4da4d18bd149ff7c1
Mateusz Rozenkiewicz <rodzyn22@gmail.com>
2020-09-14 23:05:14 +0200

    add get 'href' attribure from link

--- js/script.js
+++ js/script.js
@@ -9,10 +9,9 @@ const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    console.log('clickedElement:', clickedElement);
-    clickedElement.classList.add('active');
+    
 
-
+    
  /*[DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
@@ -23,14 +22,16 @@ for(let activeLink of activeLinks){
 
   /*[DONE] add class 'active' to the clicked link */
 
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
   /*[DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.post');
-
-for(let activeArticle of activeArticles)
+    
+for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-
+}
   /*[DONE] get 'href' attribute from the clicked link */
 
 const articleSelector = clickedElement.getAttribute('href');
@@ -42,8 +43,11 @@ const targetArticle = document.querySelector(articleSelector);
     console.log('Article: ', targetArticle);
 
   /* add class 'active' to the correct article */
+      
+console.log('targetArticle: ',targetArticle);
+targetArticle.classList.add('active');
 
-}
+};
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
